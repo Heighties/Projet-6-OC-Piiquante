a4611d3954a7fcdac6e6466758ed9770c1d996d4
Heighties <heighties@gmail.com>
2022-11-12 14:26:49 +0100

    end

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const limiter = rateLimit({
 	legacyHeaders: false,
 })
 
-// Débloquer certaines sécurité CORS, autorisation méthodes
+// Débloquer certaines sécurité CORS, en têtes utilisés, autorisation méthodes
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
